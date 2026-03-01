84e24046d63b2538aefc34c68422b9959ed9888b
Syed Abu-Talib <166926896+syed-abutalib@users.noreply.github.com>
2026-03-02 00:45:19 +0500

    Rename and update blog content cleaning function

--- src/config/cleanBlogContent.js
+++ src/config/cleanBlogContent.js
@@ -1,43 +1,61 @@
-// scripts/cleanBlogContent.js
+// scripts/fixBlogContent.js
 import mongoose from 'mongoose';
 import Blog from '../models/Blog.js';
 
-async function cleanAllBlogs() {
+async function fixAllBlogContent() {
   try {
     const blogs = await Blog.find({});
     
     for (const blog of blogs) {
       let updated = false;
       
-      if (blog.description && blog.description.includes('\\u')) {
+      if (blog.description && 
+          (blog.description.includes('\\u003c') || blog.description.includes('\\\\u003c'))) {
+        
+        console.log(`Fixing blog: ${blog.slug}`);
+        
+        // Fix description
         blog.description = blog.description
+          .replace(/\\\\u003c/g, '<')
+          .replace(/\\\\u003e/g, '>')
+          .replace(/\\\\u0022/g, '"')
+          .replace(/\\\\u002f/g, '/')
           .replace(/\\u003c/g, '<')
           .replace(/\\u003e/g, '>')
           .replace(/\\u0022/g, '"')
           .replace(/\\u002f/g, '/')
           .replace(/\\/g, '');
+        
         updated = true;
       }
       
-      if (blog.excerpt && blog.excerpt.includes('\\u')) {
+      if (blog.excerpt && 
+          (blog.excerpt.includes('\\u003c') || blog.excerpt.includes('\\\\u003c'))) {
+        
         blog.excerpt = blog.excerpt
+          .replace(/\\\\u003c/g, '<')
+          .replace(/\\\\u003e/g, '>')
+          .replace(/\\\\u0022/g, '"')
           .replace(/\\u003c/g, '<')
           .replace(/\\u003e/g, '>')
           .replace(/\\/g, '');
+        
         updated = true;
       }
       
       if (updated) {
         await blog.save();
-        console.log(`Cleaned blog: ${blog.slug}`);
+        console.log(`✅ Fixed blog: ${blog.slug}`);
       }
     }
     
-    console.log('All blogs cleaned successfully');
+    console.log('🎉 All blogs fixed successfully');
   } catch (error) {
-    console.error('Error cleaning blogs:', error);
+    console.error('Error fixing blogs:', error);
+  } finally {
+    mongoose.disconnect();
   }
 }
 
-// Run this once to clean your database
-cleanAllBlogs();
+// Run this
+fixAllBlogContent();
